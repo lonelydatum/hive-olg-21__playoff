fb54c9a9471e33a4d822bb58759d901909c260a0
Gar <gar@lonelydatu.com>
2019-04-17 10:29:12 -0400

    ss

--- dev/std_728x90/_bundled/log-free.js
+++ dev/std_728x90/_bundled/log-free.js
@@ -36,14 +36,14 @@ var ss = {
 		to: "copyToBall",
 		total: 10,
 		width: 110,
-		repeat: 3
+		repeat: 5
 	},
 	puck: {
 		from: "copyFromPuck",
 		to: "copyToPuck",
 		total: 10,
 		width: 130,
-		repeat: 1
+		repeat: 5
 	}
 };
 
@@ -53,21 +53,27 @@ function start() {
 	var tlPuck = (0, _commonJsCommonJs.makeSS)(ss.puck);
 	var tlBall = (0, _commonJsCommonJs.makeSS)(ss.ball);
 	var tlSS = new TimelineMax();
-	tlSS.to(".mask.puck", .15, { opacity: 0 }, "+=1.2");
+	tlSS.to(".mask.puck", .15, { opacity: 0 }, 1);
 
-	tlSS.add("showBall", "-=.2");
+	tlSS.add("showBall", 1);
 	tlSS.set(".mask.ball", { zIndex: 1, width: 0, opacity: 0 }, "showBall");
 	tlSS.to(".mask.ball", .3, { width: 140, opacity: 1 }, "showBall");
+
+	tlSS.add("showPuck", 3.5);
+	tlSS.to(".mask.ball", .15, { opacity: 0 }, "showPuck");
+	tlSS.set(".mask.puck", { zIndex: 1, width: 0, opacity: 0 }, "showPuck");
+	tlSS.to(".mask.puck", .3, { width: 140, opacity: 1 }, "showPuck");
 
 	tl.to(".logoHolder", 3.5, { scale: .196, x: -234, y: -176, ease: Power4.easeInOut }, "+=.8");
 	tl.add("t1", "-=1.2");
 	tl.from(".t1a", .5, { scale: 2, opacity: 0, ease: Power4.easeInOut }, "t1");
 	tl.from(".t1b", .5, { scale: 2, opacity: 0, ease: Power4.easeInOut }, "t1+=.8");
-	tl.to([".white-circle, .mask.ball"], .3, { opacity: 0 }, "t1");
+
 	tl.add("t2", "+=1.2");
 	tl.to(".logoHolder", .5, { scale: .118, x: -269, y: -189, ease: Power4.easeInOut }, "t2");
 	tl.to(".t1", .3, { opacity: 0, ease: Sine.easeOut }, "t2");
 	tl.from(".t2", .3, { opacity: 0, ease: Sine.easeOut }, "t2+=.4");
+	tl.to([".white-circle, .holder-ball"], .3, { opacity: 0 }, "t2+=.1");
 
 	tl.add("footer");
 	tl.from(".logos", .3, { opacity: 0, ease: Sine.easeOut }, "footer");
